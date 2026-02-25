efc2c10842902db520e18c0b1c74a4a63cced9b4
Jens Bech-Sørensen <jens@bechsor.no>
2026-02-25 21:10:54 +0100

    table

--- db/migrations/0007_opening_balances.sql
+++ db/migrations/0007_opening_balances.sql
@@ -1,4 +1,7 @@
 ALTER TABLE "loans" ADD COLUMN "opening_balance_oere" integer;
+--> statement-breakpoint
 ALTER TABLE "loans" ADD COLUMN "opening_balance_date" date;
+--> statement-breakpoint
 ALTER TABLE "accounts" ADD COLUMN "opening_balance_oere" integer;
+--> statement-breakpoint
 ALTER TABLE "accounts" ADD COLUMN "opening_balance_date" date;
